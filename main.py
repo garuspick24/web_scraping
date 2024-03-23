bf7dd21b05504985511e3048d0a018eee3420a7a
Andrii Homeniuk <andreygomenuk@gmail.com>
2024-03-24 01:04:14 +0200

    2

--- main.py
+++ main.py
@@ -14,3 +14,37 @@ for link in links:
         title = link.text
         print(title)
 
+city = soup.find_all('li', class_='location')
+for i in city:
+    if i:
+        title = i.text
+        print(title.strip())
+
+pirms = soup.find_all('li', class_='added')
+for i in pirms:
+    if i:
+        title = i.text
+        print(title.strip())
+
+salary = soup.find_all('li', class_="salary")
+for i in salary:
+    if i:
+        title = i.text
+        print(title.strip())
+
+company = soup.find_all('li', class_="company")
+for i in company:
+    if i:
+        title = i.text
+        print(title.strip())
+
+day_date = soup.find_all('li', class_="duedate")
+for i in day_date:
+    if i:
+        title = i.text
+        print(title.strip())
+
+
+
+
+
