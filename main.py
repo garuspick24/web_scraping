d47a71b2b56bd09737c024306f2c398989f086f2
Andrii Homeniuk <andreygomenuk@gmail.com>
2024-03-23 22:59:58 +0200

    1

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from bs4 import BeautifulSoup
 url = 'https://www.visidarbi.lv/darba-sludinajumi?page=1#results'
 response = requests.get(url)
 html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+# Приклад: знаходження всіх елементів з тегом <a>
+links = soup.find_all('a')
+for link in links:
+    print(link.get('href'))
