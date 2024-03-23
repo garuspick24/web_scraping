950f32ef0db129fde3b73e52a6a3e496f5136415
garuspick24 <122586302+garuspick24@users.noreply.github.com>
2024-03-23 22:51:43 +0200

    import and url

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("start")
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.visidarbi.lv/darba-sludinajumi?page=1#results'
+response = requests.get(url)
+html = response.text
