80a5fcc38c4c8901ea8dfa30b4fc46302666e9c4
Andrii Homeniuk <andreygomenuk@gmail.com>
2024-03-24 03:06:24 +0200

    database

--- main.py
+++ main.py
@@ -35,18 +35,22 @@ soup = BeautifulSoup(html, 'html.parser')
 # parsing data from each advertisement on one page
 counter = 0
 for title_index in range(len(soup.select(".title"))):
-
+    all_information = []
     if soup.select(".title")[title_index].select("a"):
         title_ads = (soup.select(".title")[title_index].select("a")[0].text.strip())
+        all_information.append(title_ads)
 
         counter += 1
 
         soup.select(".title")[title_index].select("a")[0].get('href')
         if soup.select(".details")[title_index].select("li"):
             for index in range(5):
+                details = soup.select(".details")[title_index].select("li")[index].text.strip()
                 print(soup.select(".details")[title_index].select("li")[index].text.strip())
+                all_information.append(details)
+    title, city, time_, salary, company, due_date = all_information
 
-    job_ad = (title_ads, '/darba-sludinajums/brugetaji-demontazas-darbinieki/65043e6ae08c1')
+    job_ad = (title, city, time_, salary, company, due_date)
 
     cursor.execute('INSERT INTO job_ads (title, link) VALUES (?, ?)', job_ad)
     conn.commit()
