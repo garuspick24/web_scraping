f113ceffc22cc0feed2392d1a9af5be0a76eea51
garuspick24 <122586302+garuspick24@users.noreply.github.com>
2024-03-24 09:16:07 +0200

    done

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sqlite3
 import requests
 from bs4 import BeautifulSoup
 
-
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
@@ -19,45 +18,43 @@ cursor.execute('''
 ''')
 conn.commit()
 
+page = 1
 
+while True:
+    url = f'https://www.visidarbi.lv/darba-sludinajumi?page={page}#results'
+    response = requests.get(url)
+    html = response.text
 
+    soup = BeautifulSoup(html, 'html.parser')
+    if len(soup.select(".title")):
 
+        counter = 0
+        for title_index in range(len(soup.select(".title"))):
+            all_information = [None, None, None, None, None, None]
+            if soup.select(".title")[title_index].select("a"):
+                title_ads = (soup.select(".title")[title_index].select("a")[0].text.strip())
+                all_information[0] = title_ads
 
+                counter += 1
 
+                soup.select(".title")[title_index].select("a")[0].get('href')
+                if soup.select(".details")[title_index].select("li"):
+                    for index in range(5):
+                        details = soup.select(".details")[title_index].select("li")[index].text.strip()
+                        if details:
+                            # print(soup.select(".details")[title_index].select("li")[index].text.strip())
+                            all_information[index + 1] = details
 
-url = 'https://www.visidarbi.lv/darba-sludinajumi?page=1#results'
-response = requests.get(url)
-html = response.text
+                title, city, time_, salary, company, due_date = all_information
 
-soup = BeautifulSoup(html, 'html.parser')
+                job_ad = (title, city, time_, salary, company, due_date)
+                cursor.execute(
+                    'INSERT INTO job_ads (title, city, time, salary, company, due_date) VALUES (?, ?, ?, ?, ?, ?)',
+                    job_ad)
+                conn.commit()
+                # print("---------------------------------------------------")
 
-# parsing data from each advertisement on one page
-counter = 0
-for title_index in range(len(soup.select(".title"))):
-    all_information = [None, None, None, None, None, None]
-    if soup.select(".title")[title_index].select("a"):
-        title_ads = (soup.select(".title")[title_index].select("a")[0].text.strip())
-        all_information[0] = title_ads
-
-        counter += 1
-
-        soup.select(".title")[title_index].select("a")[0].get('href')
-        if soup.select(".details")[title_index].select("li"):
-            for index in range(5):
-                details = soup.select(".details")[title_index].select("li")[index].text.strip()
-                if details:
-                    print(soup.select(".details")[title_index].select("li")[index].text.strip())
-                    all_information[index + 1] = details
-
-        title, city, time_, salary, company, due_date = all_information
-
-        job_ad = (title, city, time_, salary, company, due_date)
-        cursor.execute('INSERT INTO job_ads (title, city, time, salary, company, due_date) VALUES (?, ?, ?, ?, ?, ?)', job_ad)
-        conn.commit()
-        print("---------------------------------------------------")
-
-
-print(counter)
+    page += 1
 
 # Закриття підключення
 conn.close()
